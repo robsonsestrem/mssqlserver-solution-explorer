ec0fe0fb8b6c68d8b876c0a9c966a4272ac3b716
OSIRIS-DATACENT\SYSADMIN <sestrem.robson@gmail.com>
2025-03-24 19:36:39 -0300

    * New general adjustments;

--- SQL-EXPERIENCE/TROUBLESHOOTING-LOGS/ConsultaLogSp_readerrorlog.sql
+++ SQL-EXPERIENCE/TROUBLESHOOTING-LOGS/ConsultaLogSp_readerrorlog.sql
@@ -9,10 +9,10 @@ EXEC sp_readerrorlog; -- função interna
 SELECT
     *
 FROM @logs AS l
-WHERE l.data >= '20230809 00:00:00.000' AND l.data <= '20230809 16:00:00.000'
+WHERE l.data >= '20250310 06:00:00.000' AND l.data <= '20250310 16:00:00.000'
 --AND l.data <= '20240507 23:59:59.997'
 --AND l.[Text] NOT LIKE '%Login failed%' AND l.[Text] NOT LIKE '%Error: 18456, Severity: 14, State: 8.%' -- Login failed; Error: 18456, Severity: 14, State: 8.; Process ID 823 was killed by hostname HMNOT005, host process ID 15956;
---AND l.Text LIKE '%was killed%'
+AND l.Text LIKE '%was killed%'
 --AND l.Text NOT LIKE '%Error: 18456%'
 -- 
 ORDER BY l.data DESC
